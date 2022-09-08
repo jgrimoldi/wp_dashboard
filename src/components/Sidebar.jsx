165e4d8238f8d0af236e5c2ae35df81d13d2dba7
jgrimoldi <jgrimoldi@frba.utn.edu.ar>
2022-09-08 15:12:21 -0300

    settingUp

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+const Sidebar = () => {
+  const activeMenu = true;
+
+
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default Sidebar
---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+export { default as Dashboard } from './Dashboard';
+export { default as Storage } from './Storage';
+export { default as ProductType } from './Products/ProductType';
+export { default as Units } from './Products/Units';
+export { default as Products } from './Products/Products';
+export { default as Clients } from './Clients';
+export { default as Category } from './Suppliers/Category';
+export { default as Providers } from './Suppliers/Providers';
+export { default as Income } from './Income';
+export { default as Expenses } from './Expenses';
+export { default as Transfer } from './Transfer';
+export { default as Stats } from './Stats';
+export { default as Reports } from './Reports';
+export { default as Help } from './Help';
+export { default as Register } from './Management/Register';
+export { default as Settings } from './Management/Settings';
+export { default as Backup } from './Management/Backup';
