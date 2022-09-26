ba172d98dd6e97fe09aaf3fba8d0589f87577f73
jgrimoldi <jgrimoldi@frba.utn.edu.ar>
2022-09-26 19:09:46 -0300

    refactor homeNavigation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomeNavigation } from './components';
+
+import { Dashboard, Storage, Clients, Products, ProductType, Units, Providers, Category, Income, Expenses, Transfer, Stats, Reports, Register, Settings, Backup, Help, Login, ForgotPassword, ResetPassword, AccountValidation } from './pages';
+
 import './App.css';
 
 
 const App = () => {
 
   return (
-    <div>
-      <BrowserRouter>
-        <HomeNavigation />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+
+        {/* login */}
+        <Route path='/' element={(<Login />)} />
+        <Route path='/inicio' element={(<Login />)} />
+        <Route path='/recuperacion' element={<ForgotPassword />} />
+        <Route path='/restauracion' element={<ResetPassword />} />
+        <Route path='/validacion' element={<AccountValidation />} />
+
+
+        <Route element={(<HomeNavigation />)}>
+          {/* dashboard */}
+          <Route path='/' element={(<Dashboard />)} />
+          <Route path='/dashboard' element={(<Dashboard />)} />
+          <Route path='/almacen' element={<Storage />} />
+          <Route path='/clientes' element={<Clients />} />
+          {/* products */}
+          <Route path='/productos' element={<Products />} />
+          <Route path='/tipo-de-productos' element={<ProductType />} />
+          <Route path='/unidades-de-medida' element={<Units />} />
+          {/* suppliers */}
+          <Route path='/proveedores' element={<Providers />} />
+          <Route path='/categoria-de-proveedores' element={<Category />} />
+          {/* moves */}
+          <Route path='/ingresos' element={<Income />} />
+          <Route path='/egresos' element={<Expenses />} />
+          <Route path='/transferencia-entre-almacenes' element={<Transfer />} />
+          {/* reports */}
+          <Route path='/estadisticas' element={<Stats />} />
+          <Route path='/reportes' element={<Reports />} />
+          {/* management */}
+          <Route path='/registro' element={<Register />} />
+          <Route path='/perfil' element={<Settings />} />
+          <Route path='/restaurar' element={<Backup />} />
+
+          <Route path='/ayuda' element={<Help />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/components/HomeNavigation.jsx
+++ src/components/HomeNavigation.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Header, Sidebar } from '../components';
-
-import { Dashboard, Storage, Clients, Products, ProductType, Units, Providers, Category, Income, Expenses, Transfer, Stats, Reports, Register, Settings, Backup, Help } from '../pages';
 
 import { useStateContext } from '../contexts/ContextProvider';
 
@@ -28,33 +26,7 @@ const HomeNavigation = () => {
                 </div>
 
                 <div>
-                    <Routes>
-                        {/* dashboard */}
-                        <Route path='/' element={(<Dashboard />)} />
-                        <Route path='/dashboard' element={(<Dashboard />)} />
-                        <Route path='/almacen' element={<Storage />} />
-                        <Route path='/clientes' element={<Clients />} />
-                        {/* products */}
-                        <Route path='/productos' element={<Products />} />
-                        <Route path='/tipo-de-productos' element={<ProductType />} />
-                        <Route path='/unidades-de-medida' element={<Units />} />
-                        {/* suppliers */}
-                        <Route path='/proveedores' element={<Providers />} />
-                        <Route path='/categoria-de-proveedores' element={<Category />} />
-                        {/* moves */}
-                        <Route path='/ingresos' element={<Income />} />
-                        <Route path='/egresos' element={<Expenses />} />
-                        <Route path='/transferencia-entre-almacenes' element={<Transfer />} />
-                        {/* reports */}
-                        <Route path='/estadisticas' element={<Stats />} />
-                        <Route path='/reportes' element={<Reports />} />
-                        {/* management */}
-                        <Route path='/registro' element={<Register />} />
-                        <Route path='/perfil' element={<Settings />} />
-                        <Route path='/restaurar' element={<Backup />} />
-
-                        <Route path='/ayuda' element={<Help />} />
-                    </Routes>
+                    <Outlet />
                 </div>
 
             </div>
